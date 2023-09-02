def98f3ceeb2ff7fd9c5f97ac30f9344ead268e0
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-09-02 18:42:08 -0400

    changing things

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-function iniciar() {
+const horaEl = document.querySelector("#hora");
+const minutosEl = document.querySelector("#minutos");
+const segundosEl = document.querySelector("#segundos");
+const iniciarBtn = document.querySelector("#iniciar");
+const pararBtn = document.querySelector("#parar");
+const resetarBtn = document.querySelector("#resetar");
+
+let intervalo;
+let hora = 0;
+let minutos = 0;
+let segundos = 0;
+let milisegundos = 0;
+let pausado = false;
+
+iniciarBtn.addEventListener("click", iniciarContador);
+pararBtn.addEventListener("click", pararTempo);
+
+function iniciarContador() {
+
+    intervalo = setInterval(() => {
+
+        if (!pausado) {
+
+            segundos += 1;
+
+            if (segundos === 60) {
+                minutos++;
+                segundos = 0;
+            }
+
+            if (minutos === 60) {
+                hora++;
+                minutos = 0;
+            }
+
+            minutosEl.textContent = formatarTempo(minutos);
+            segundosEl.textContent = formatarTempo(segundos);
+            horaEl.textContent = formatarTempo(hora);
+
+        }
+
+    }, 1000);
 
 }
 
-function resetar() {
+function pararTempo(){
+    pausado = true;
 
 }
 
-function parar() {
-
-}
-
-function incrementarHora(){
-
-}
-
-function incrementarMinutos() {
-
-}
-
-function incrementarContador() {
-    let contador = 0;
-    if (contador == 59) {
-        contador = 0;
-        incrementarMinutos();
+function formatarTempo(tempo) {
+    if (tempo < 10){
+        return `0${tempo}`;
     } else {
-        contador += 1;
+        return tempo;
     }
-    
-
-    return contador;
 }
-
-hora = parseInt(document.getElementById("hora").value);
-minutos = parseInt(document.getElementById("minutos").value);
-segundos = parseInt(document.getElementById("segundos").value);
 
 console.log(setInterval(incrementarContador(), 1000));
